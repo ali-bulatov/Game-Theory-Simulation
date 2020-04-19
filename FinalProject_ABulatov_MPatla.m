ca09d1c66f2614ec451b1b8c7883710eae8d441a
Ali <bulatova@sheridancollege.ca>
2020-04-19 17:00:18 -0400

    Update FinalProject_ABulatov_MPatla.m
    Cell neighbors vector added.

--- FinalProject_ABulatov_MPatla.m
+++ FinalProject_ABulatov_MPatla.m
@@ -12,7 +12,8 @@ up=[2:automaton_dimensions 1];
 down=[automaton_dimensions 1:automaton_dimensions-1];
 
 for i=1:100000
-    
+    neighbours=GRID(up,:)+GRID(down,:)+GRID(:,up)+GRID(:,down)+...
+        GRID(up,up)+GRID(up,down)+GRID(down,up)+GRID(down,down);
     
     pause(0.5);
     if ~ishghandle(h_fig)   % Quit if user closes the figure window
