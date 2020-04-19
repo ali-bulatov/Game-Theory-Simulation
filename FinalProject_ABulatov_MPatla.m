c206505d930568ebc7e822e9c67e991f58f59bf4
Ali <bulatova@sheridancollege.ca>
2020-04-19 16:59:38 -0400

    Automaton grid created.

--- FinalProject_ABulatov_MPatla.m
+++ FinalProject_ABulatov_MPatla.m
@@ -3,4 +3,19 @@
 % Written for INFO48874
 % April 17th 2020
 
+% Automaton grid automaton_dimensions X automaton_dimensions
+% Cooperator = 0, Defector = 1
+automaton_dimensions=50;
+GRID=int8(rand(automaton_dimensions,automaton_dimensions));
+% Cells up and down 
+up=[2:automaton_dimensions 1];
+down=[automaton_dimensions 1:automaton_dimensions-1];
 
+for i=1:100000
+    
+    
+    pause(0.5);
+    if ~ishghandle(h_fig)   % Quit if user closes the figure window
+        break;
+    end
+end
