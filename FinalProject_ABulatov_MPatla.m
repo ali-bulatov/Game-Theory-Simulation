8233e4c4a1ae34a4ecc5c7718ee844f910c4c2d2
Ali <bulatova@sheridancollege.ca>
2020-04-19 20:07:13 -0400

    Added scrore accumulation logic

--- FinalProject_ABulatov_MPatla.m
+++ FinalProject_ABulatov_MPatla.m
@@ -4,49 +4,66 @@
 % April 17th 2020
 
 clear;
- % Seed, change this (or remove) for different initial conditions
-rng(2);  
-h_fig = figure;
-% PAYOFF MATRIX
-% Reward for cheating
-TEMPTATION = 1;
-% Sucker's payoff - when you cooperate and other one cheats
-S_PAYOFF = 0;
-% Reward for both cooperating
-REWARD = 1;
-% Both players cheat
-PUNISHMENT = 0;
+clc;
+main();
 
-% Automaton grid automaton_dimensions X automaton_dimensions
-% Cooperator = 0, Defector = 1
-automaton_dimensions=50;
-GRID=int8(rand(automaton_dimensions,automaton_dimensions));
-% Cells up and down 
-up=[2:automaton_dimensions 1];
-down=[automaton_dimensions 1:automaton_dimensions-1];
+function main()
+     % Seed, change this (or remove) for different initial conditions
+    rng(2);  
+    h_fig = figure;
+    % PAYOFF MATRIX
+    % Reward for cheating
+    TEMPTATION = 1;
+    % Sucker's payoff - when you cooperate and other one cheats
+    S_PAYOFF = 0;
+    % Reward for both cooperating
+    REWARD = 1;
+    % Both players cheat
+    PUNISHMENT = 0;
 
-for i=1:100000
-    neighbours=GRID(up,:)+GRID(down,:)+GRID(:,up)+GRID(:,down)+...
-        GRID(up,up)+GRID(up,down)+GRID(down,up)+GRID(down,down);
-    
-    pause(0.5);
-    if ~ishghandle(h_fig)   % Quit if user closes the figure window
-        break;
+    % Automaton grid automaton_dimensions X automaton_dimensions
+    % Cooperator = 0, Defector = 1
+    l=3;
+    GRID=int8(rand(l,l));
+    % Score matrix with scores of each player
+    SCORE=(zeros(l));
+
+    for i=1:1
+        % 1. each agent accumulates the payoff obtained by playing
+        %  the game with all its neighbours and itself
+        for idx = 1:numel(GRID)
+            X = GRID(idx);
+            score = playGame(X, X);
+            
+            for n = 1:numel(N)
+                score = score + playGame(X, N);
+            end
+            SCORE(idx) = score;
+        end
+        
+        image(GRID,'CDataMapping','scaled');
+        colorbar
+        
+        pause(0.5);
+        if ~ishghandle(h_fig)   % Quit if user closes the figure window
+            break;
+        end
     end
-end
 
-function [m] = PlayGame(x,y)
-    rewardType = x * 2 + y;
-    switch rewardType
-        case 0 
-            m = REWARD;  % CC: Reward for mutual cooperation
-        case 1
-            m = S_PAYOFF;  % CD: Sucker's payoff 
-        case 2
-            m = TEMPTATION;  % DC: Temptation to defect
-        case 3
-            m = PUNISHMENT;  % DD: Punishment for mutual defection
-        otherwise
-            disp("Strategy should be either 0 or 1.");
+    function [m] = playGame(x,y)
+        rewardType = x * 2 + y;
+        switch rewardType
+            case 0 
+                m = REWARD;  % CC: Reward for mutual cooperation
+            case 1
+                m = S_PAYOFF;  % CD: Sucker's payoff 
+            case 2
+                m = TEMPTATION;  % DC: Temptation to defect
+            case 3
+                m = PUNISHMENT;  % DD: Punishment for mutual defection
+            otherwise
+                m = "Error";
+                disp("Strategy should be either 0 or 1.");
+        end
     end
 end
